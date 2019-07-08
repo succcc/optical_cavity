c14f434bd2c823d7bcd153ce4f555a4bcabf8b28
hdsnCal <khdsn97@gmail.com>
2019-07-08 20:43:42 +0900

    variables fix

--- cavity.py
+++ cavity.py
@@ -29,20 +29,21 @@ class Cavity:
         self.d = d
         self.lamb = lamb
         self.I0 = I0
-        self.i0 = self.T1*I0
+        self.I0_in = self.T1*I0  # Initial intensity inside cavity
+        self.E0 = np.sqrt(I0)
 
         self.nu0 = c / self.lamb
         self.W0 = np.power((self.lamb / np.pi) ** 2 * (
                 -self.d * (self.roc1 + d) * (self.roc2 + self.d) * (self.roc1 + self.roc2 + self.d))
                            / (self.roc1 + self.roc2 + 2 * self.d) ** 2, 1 / 4)
         self.z0 = np.pi * self.W0 ** 2 / self.lamb
-        self.FSR = 3e8 / 2 / self.d
+        self.FSR = c / 2 / self.d
         self.g1g2 = (1 + self.d / self.roc1) * (1 + self.d / self.roc2)
         self.finesse = np.pi * (R1 * R2) ** (1 / 4) / (1 - np.sqrt(R1 * R2))
         self.a_s = 0  # Assuming no attenuation in cavity, only consider reflectivity of mirrors
 
         self.ar = self.a_s + 1 / (2 * d) * np.log(1 / (R1 * R2))
-        self.absr = np.sqrt(np.exp(-2*self.ar*self.d))
+        self.r_abs = np.sqrt(np.exp(-2*self.ar*self.d))
 
         self.finesse2 = np.pi * np.exp(-self.ar * d / 2) / (1 - np.exp(-self.ar * d))
         self.delta_nu = self.FSR / self.finesse  # approx
@@ -51,18 +52,18 @@ class Cavity:
         self.qFactor = self.nu0 / self.delta_nu
         self.qFactor_correct = 2 * np.pi * self.nu0 / (c * self.ar)
 
-        self.imax = self.i0/((1-self.absr)**2)
+        self.I_max = self.I0_in/((1-self.r_abs)**2)
 
-    def intensity(self, freq):
-        return self.imax/(1 + (2*self.finesse/np.pi)**2 * np.sin(np.pi*freq/self.FSR)**2)
+    def intensity_i(self, freq):
+        return self.I_max/(1 + (2*self.finesse/np.pi)**2 * np.sin(np.pi*freq/self.FSR)**2)
 
-    def setD(self, d):
+    def set_d(self, d):
         self.__init__(self.roc1, self.roc2, self.pos1, d, self.R1, self.R2, self.lamb, self.I0)
 
-    def setPos1(self, pos1):
-        self.__init__(self.roc1, self.roc2, pos1, self.d, self.r1, self.r2, self.lamb, self.I0)
+    def set_pos1(self, pos1):
+        self.__init__(self.roc1, self.roc2, pos1, self.d, self.R1, self.R2, self.lamb, self.I0)
 
-    def getSpectrum(self, wStart=None, wEnd=None, fStart=None, fEnd=None, wCenter=None, fWidth=None, num=1000):
+    def get_response(self, wStart=None, wEnd=None, fStart=None, fEnd=None, wCenter=None, fWidth=None, num=1000):
         isWavelength = wStart or wEnd
         isFrequencyIntv = fStart or fEnd
         isWavelengthCenter = wCenter or fWidth
@@ -76,7 +77,7 @@ class Cavity:
         else:
             print("Please check the input variables...")
             return 0, 0
-        return freq, self.intensity(freq)
+        return freq, self.intensity_i(freq)
 
     def report(self):
         print(c)
--- main.py
+++ main.py
@@ -22,12 +22,12 @@ um = 1e-6
 nm = 1e-9
 
 c1 = Cavity(roc1=-10*mm, roc2=-10*mm, pos1=0, d=10.001*mm, R1=0.98, R2=0.98, lamb=1042*nm, I0=1)
-c1Spectrum = c1.getSpectrum(wCenter=1042 * nm, fWidth=30 * ghz, num=100000)
+c1Spectrum = c1.get_response(wCenter=1042 * nm, fWidth=30 * ghz, num=100000)
 plt.plot((c1Spectrum[0]-c/c1.lamb)/ghz,
          c1Spectrum[1])
 
 c2 = Cavity(roc1=-10000000.0, roc2=-10.0*mm, pos1=0, d=5.0*mm, R1=0.99, R2=0.99, lamb=1042*nm, I0=1)
-c2Spectrum = c2.getSpectrum(wCenter=1042 * nm, fWidth=30 * ghz, num=100000)
+c2Spectrum = c2.get_response(wCenter=1042 * nm, fWidth=30 * ghz, num=100000)
 plt.plot((c2Spectrum[0]-c/c2.lamb)/ghz,
          c2Spectrum[1])
 
